fd0eba357b5b189f10aa31f807320ed49c8fb0bb
Elijah Walker <elijahtruthwalker05@gmail.com>
2024-11-08 20:48:46 -0600

    Created the React App Home Page

---
+++ invest_insight/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">InsightInvest</h1>
+      <p className="subtitle">Trade Smarter, Stay Informed.</p>
+      <div className="button-container">
+        <button className="button">Login</button>
+        <button className="button">Create Account</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
